9eb1d30569fc8023e30fe8ade6413b8083a9b775
harebit <hare62.web.de>
2018-05-27 14:26:59 +0200

    start with original

---
+++ friend/Inc/graphictest.hpp
@@ -0,0 +1,63 @@
+// All the mcufriend.com UNO shields have the same pinout.
+// i.e. control pins A0-A4.  Data D2-D9.  microSD D10-D13.
+// Touchscreens are normally A1, A2, D7, D6 but the order varies
+//
+// This demo should work with most Adafruit TFT libraries
+// If you are not using a shield,  use a full Adafruit constructor()
+// e.g. Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+#ifndef _GRAPHICTEST_HPP
+#define _GRAPHICTEST_HPP
+
+#include <Adafruit_GFX.hpp> // Hardware-specific library
+#include "clock.h"
+#include "MCUFRIEND_kbv.hpp"
+#include "wiring_time.h"
+
+/*#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin*/
+
+#define F_CPU 64000000
+
+// Assign human-readable names to some common 16-bit color values:
+#define	BLACK   0x0000
+#define	BLUE    0x001F
+#define	RED     0xF800
+#define	GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+#ifndef min
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#endif
+
+void setup(void);
+void loop(void);
+
+unsigned long testFillScreen();
+unsigned long testText();
+unsigned long testLines(uint16_t color);
+unsigned long testFastLines(uint16_t color1, uint16_t color2);
+unsigned long testRects(uint16_t color);
+unsigned long testFilledRects(uint16_t color1, uint16_t color2);
+unsigned long testFilledCircles(uint8_t radius, uint16_t color);
+unsigned long testCircles(uint8_t radius, uint16_t color);
+unsigned long testTriangles();
+unsigned long testFilledTriangles();
+unsigned long testRoundRects();
+unsigned long testFilledRoundRects();
+void progmemPrint(const char *str);
+void progmemPrintln(const char *str);
+
+void runtests(void);
+
+
+
+extern const uint8_t hanzi[];
+
+#endif //GRAPHICTEST.HPP
